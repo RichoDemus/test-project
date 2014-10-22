85fc2602ef7c86d76a932b841c288924fc2933f0
RichoDemus <richodemus@gmail.com>
2014-10-22 17:10:45 +0200

    more work

--- src/main/java/richo/testproject/jms/apistuff/implementation/server/JmsToApiBridge.java
+++ src/main/java/richo/testproject/jms/apistuff/implementation/server/JmsToApiBridge.java
@@ -1,10 +1,17 @@
 package richo.testproject.jms.apistuff.implementation.server;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import richo.testproject.jms.apistuff.api.Api;
 
 public class JmsToApiBridge
 {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private final Api apiImplementation;
+
+	public JmsToApiBridge(@NotNull final Api apiImplementation)
+	{
+		this.apiImplementation = apiImplementation;
+	}
 }
