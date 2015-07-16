16a164bedc54c78093e562e116665980f766c5fb
Richard Tjerngren <richard.tjerngren@netnet.com>
2015-07-16 13:22:44 +0200

    Converting a reactive stream of lists into their entries

--- src/main/java/richo/testproject/reactive/simpleobserver/Client.java
+++ src/main/java/richo/testproject/reactive/simpleobserver/Client.java
@@ -1,5 +1,6 @@
 package richo.testproject.reactive.simpleobserver;
 
+import rx.Observable;
 import rx.Subscriber;
 
 public class Client
@@ -14,6 +15,9 @@ public class Client
                 .map(String::toUpperCase)
                 .filter(s -> !s.startsWith("W"))
                 .subscribe(s -> System.out.println("Got message : " + s));
+
+        listToEntityStuff(api);
+
 
         api.getSequence()
                 .skip(1)
@@ -43,5 +47,42 @@ public class Client
                         }
                     }
                 });
+    }
+
+    /**
+     * Some experiments on how to convert a stream of lists (which you get when you buffer) back to the entities that were buffered
+     *
+     * @param api yadda yadda
+     */
+    private void listToEntityStuff(Api api)
+    {
+        api.getSequence()
+                .takeUntil(l -> l >= 90)
+                .filter(l -> l % 10 == 0)
+                .buffer(10)
+                .flatMapIterable(longs -> longs)
+                .flatMap(Observable::just)
+                .subscribe(l -> System.out.println("Buffered Sequence using two flatmaps: " + l));
+
+        api.getSequence()
+                .takeUntil(l -> l >= 90)
+                .filter(l -> l % 10 == 0)
+                .buffer(10)
+                .compose(iterableObservable -> iterableObservable.flatMap(Observable::from))
+                .subscribe(l -> System.out.println("Buffered Sequence with compose containing flatmap: " + l));
+
+        api.getSequence()
+                .takeUntil(l -> l >= 90)
+                .filter(l -> l % 10 == 0)
+                .buffer(10)
+                .compose(iterableObservable -> iterableObservable.flatMap(Observable::from))
+                .subscribe(l -> System.out.println("Buffered Sequence with flatmap and compose: " + l));
+
+        api.getSequence()
+                .takeUntil(l -> l >= 90)
+                .filter(l -> l % 10 == 0)
+                .buffer(10)
+                .flatMap(Observable::from)
+                .subscribe(l -> System.out.println("Buffered Sequence with just a flatmap: " + l));
     }
 }
